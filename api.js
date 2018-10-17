db390c0c8d09387da48706839aee47785ba11d52
Loboconcepts <adam@loboconcepts.com>
2018-10-17 12:24:58 -0400

    api

---
+++ api.js
@@ -0,0 +1,11 @@
+(function(){
+  const nflPlayers = 'http://api.fantasy.nfl.com/v1/players/stats?statType=seasonStats&season=2018&week=1&format=json'
+  fetch(nflPlayers)
+  	.then(function(data) {
+
+ 	})
+  	.catch(function(error) {
+
+  	})
+
+})();
