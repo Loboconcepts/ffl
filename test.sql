212208c171836cfb3825fa04a3d7dacb15c843c2
Adam <adam@loboconcepts.com>
2018-10-18 11:08:25 -0400

    sql table

---
+++ test.sql
@@ -0,0 +1,19 @@
+create table Users (
+	id INT,
+	first_name VARCHAR(50),
+	last_name VARCHAR(50),
+	email VARCHAR(50),
+	league_name VARCHAR(7),
+	team_name VARCHAR(50),
+	team VARCHAR(7)
+);
+insert into Users (id, first_name, last_name, email, league_name, team_name, team) values (1, 'Brian', 'Kay', 'ddmitriev0@ebay.co.uk', 'dolfans', 'Kautzer-Conn', '[888888]');
+insert into Users (id, first_name, last_name, email, league_name, team_name, team) values (2, 'Adam', 'Sharaf', 'bbowdon1@opera.com', 'dolfans', 'Wehner-Considine', '[888888]');
+insert into Users (id, first_name, last_name, email, league_name, team_name, team) values (3, 'Donovan', 'Timby', 'dtimby2@nbcnews.com', 'dolfans', 'Witting, Ondricka and Kuphal', '[888888]');
+insert into Users (id, first_name, last_name, email, league_name, team_name, team) values (4, 'Iris', 'Hayer', 'ihayer3@marriott.com', 'dolfans', 'Conn, Rutherford and Waelchi', '[888888]');
+insert into Users (id, first_name, last_name, email, league_name, team_name, team) values (5, 'Madge', 'Ritch', 'mritch4@oracle.com', 'dolfans', 'Mayert Group', '[888888]');
+insert into Users (id, first_name, last_name, email, league_name, team_name, team) values (6, 'Pail', 'Jewkes', 'pjewkes5@typepad.com', 'dolfans', 'Flatley-Hegmann', '[888888]');
+insert into Users (id, first_name, last_name, email, league_name, team_name, team) values (7, 'Murielle', 'Deek', 'mdeek6@va.gov', 'dolfans', 'Dickens LLC', '[888888]');
+insert into Users (id, first_name, last_name, email, league_name, team_name, team) values (8, 'Keriann', 'Sennett', 'ksennett7@deviantart.com', 'dolfans', 'Kunze-Durgan', '[888888]');
+insert into Users (id, first_name, last_name, email, league_name, team_name, team) values (9, 'Susie', 'Maciejewski', 'smaciejewski8@youku.com', 'dolfans', 'Fahey, Wolf and Runolfsdottir', '[888888]');
+insert into Users (id, first_name, last_name, email, league_name, team_name, team) values (10, 'Phyllida', 'Raitt', 'praitt9@msn.com', 'dolfans', 'Stoltenberg, Klein and Senger', '[888888]');
